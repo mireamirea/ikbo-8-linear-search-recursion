1ead2a79fc3364a03632139607cacb5a5ec9fff4
Leoner36 <Leoner36@gmail.com>
2017-04-22 22:39:22 +0300

    recursion

--- Saitov/task_1.c
+++ Saitov/task_1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int findMax (int a, int t){
+if (a>0){
+int residue=a % 10;
+a=(a-residue)/10;
+if (residue>t) t=residue;
+return (findMax(a, t));
+}
+   else return t;
+}
+
+int main()
+{
+int a=228;
+printf("The largest figure in the number:%d", findMax(a, 0));
+    return 0;
+}
